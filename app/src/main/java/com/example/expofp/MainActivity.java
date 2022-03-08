034a8af89bbf3fc70bd840d5fd77aed1c8c05d9b
Nikitin Vladimir <vladimir.nikitin@shtormtech.ru>
2022-03-08 23:57:19 +0300

    update for fplan 1.1.0 release

--- app/src/main/java/com/example/expofp/MainActivity.java
+++ app/src/main/java/com/example/expofp/MainActivity.java
@@ -4,54 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-import com.expofp.fplan.BoothSelectedCallback;
-import com.expofp.fplan.FpConfiguredCallback;
+import com.expofp.fplan.FplanEventListener;
 import com.expofp.fplan.FplanView;
 import com.expofp.fplan.Route;
-import com.expofp.fplan.RouteCreatedCallback;
 
-
-class RouteCallback implements RouteCreatedCallback {
-
-    private FplanView _fplanView;
-
-    public RouteCallback(FplanView fplanView){
-        _fplanView = fplanView;
-    }
-
-    @Override
-    public void onRouteCreated(Route route) {
-        //Some code
-    }
-}
-
-class BoothCallback implements BoothSelectedCallback {
-
-    private FplanView _fplanView;
-
-    public BoothCallback(FplanView fplanView){
-        _fplanView = fplanView;
-    }
-
-    @Override
-    public void onBoothSelected(String boothName) {
-        //Some code
-    }
-}
-
-class FpCallback implements FpConfiguredCallback{
-
-    private FplanView _fplanView;
-
-    public FpCallback(FplanView fplanView){
-        _fplanView = fplanView;
-    }
-
-    @Override
-    public void onFpConfigured() {
-        //Some code
-    }
-}
 
 public class MainActivity extends AppCompatActivity {
 
@@ -63,8 +19,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         _fplanView = findViewById(R.id.fplanView);
-        _fplanView.init("https://wayfinding.expofp.com", new BoothCallback(_fplanView), new FpCallback(_fplanView), new RouteCallback(_fplanView));
+        _fplanView.init("https://wayfinding.expofp.com", new FplanEventListener() {
+            @Override
+            public void onFpConfigured() {
 
+            }
+
+            @Override
+            public void onBoothSelected(String s) {
+
+            }
+
+            @Override
+            public void onRouteCreated(Route route) {
+
+            }
+        });
     }
 
     public void onSelectBoothClick(View view) {
