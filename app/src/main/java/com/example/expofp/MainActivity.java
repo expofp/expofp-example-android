5b4e575b443fd9c0ba9f5edd3cbccadd612182a0
Vladimir Nikitin <vladimir.n0691@gmail.com>
2024-06-12 19:01:07 +0300

    dlg-feldtage

--- app/src/main/java/com/example/expofp/MainActivity.java
+++ app/src/main/java/com/example/expofp/MainActivity.java
@@ -1,29 +1,13 @@
 package com.example.expofp;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.webkit.ValueCallback;
-
-import com.expofp.common.Location;
-
-import com.expofp.fplan.FplanEventsListener;
+import com.expofp.common.LocationProvider;
+import com.expofp.crowdconnected.Mode;
 import com.expofp.fplan.FplanView;
 
 import com.expofp.crowdconnected.CrowdConnectedProvider;
-import com.expofp.fplan.models.Bookmark;
-import com.expofp.fplan.models.Details;
-import com.expofp.fplan.models.FloorPlanBooth;
-import com.expofp.fplan.models.FloorPlanBoothBase;
-import com.expofp.fplan.models.Route;
-import com.expofp.indooratlas.IndoorAtlasProvider;
-
-import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,26 +17,6 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main_menu, menu);
         return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.action_select_booth) {
-            _fplanView.selectBooth("305");
-        } else if (id == R.id.action_select_exhibitor) {
-            _fplanView.selectExhibitor("Aria Style");
-        } else if (id == R.id.action_build_route) {
-            _fplanView.selectRoute("305", "339", false);
-        } else if (id == R.id.action_set_position) {
-            _fplanView.selectCurrentPosition(new Location(45000.00, 14000.00, null, null,
-                    null, null), true);
-        } else if (id == R.id.action_clear) {
-            _fplanView.clear();
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -66,47 +30,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        com.expofp.crowdconnected.Settings lpSettings = new com.expofp.crowdconnected.Settings("9defbd49",
+                "cd3aea7fa06a476d9cbb83a9594f5306","5c7A9fdCQ8N996O61F64Tw3n3jvol532", Mode.GPS_ONLY);
+        LocationProvider lp = new CrowdConnectedProvider(getApplication(), lpSettings);
+
         com.expofp.fplan.models.Settings settings = new com.expofp.fplan.models.Settings()
-                //.withLocationProvider(new CrowdConnectedProvider(getApplication(), new com.expofp.crowdconnected.Settings("APP_KEY","TOKEN","SECRET")))
-                //.withLocationProvider(new IndoorAtlasProvider(getApplication(), "API_KEY", "API_SECRET_KEY"))
-                //.withGlobalLocationProvider()
-                .withEventsListener(new FplanEventsListener() {
-                    @Override
-                    public void onFpConfigured() {
-                    }
-
-                    @Nullable
-                    @Override
-                    public void onFpConfigureError(int errorCode, String description) {
-                    }
-
-                    @Override
-                    public void onBoothClick(@Nullable FloorPlanBoothBase booth) {
-                    }
-
-                    @Override
-                    public void onDirection(@Nullable Route route) {
-                    }
-
-                    @Override
-                    public void onMessageReceived(@Nullable String message) {
-                    }
-
-                    @Override
-                    public void onDetails(@Nullable Details details) {
-                    }
-
-                    @Override
-                    public void onBookmarkClick(Bookmark bookmark) {
-
-                    }
-
-                    @Override
-                    public void onExhibitorCustomButtonClick(String externalId, int buttonNumber, String buttonUrl) {
-                    }
-                });
+                .withLocationProvider(lp);
 
         _fplanView = findViewById(R.id.fplanView);
-        _fplanView.init("https://demo.expofp.com", settings);
+        _fplanView.init("https://dlg-feldtage.expofp.com", settings);
     }
 }
