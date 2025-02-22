dceb0e97e5911ea596d577bf2842b7227bb5044a
Vladimir Nikitin <vladimir.n0691@gmail.com>
2025-02-23 01:26:41 +0300

    Debug offline maps

--- app/src/main/java/com/example/expofp/MainActivity.java
+++ app/src/main/java/com/example/expofp/MainActivity.java
@@ -2,30 +2,39 @@ package com.example.expofp;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.expofp.common.Location;
 import com.expofp.crowdconnected.Mode;
 import com.expofp.fplan.FplanView;
 
 import com.expofp.crowdconnected.CrowdConnectedProvider;
+import com.expofp.fplan.contracts.DownloadZipToCacheCallback;
 import com.expofp.fplan.contracts.FplanEventsListener;
 import com.expofp.fplan.models.Bookmark;
 import com.expofp.fplan.models.Category;
 import com.expofp.fplan.models.Details;
 import com.expofp.fplan.models.FloorPlanBoothBase;
 import com.expofp.fplan.models.Route;
+import com.expofp.fplan.models.Settings;
 import com.expofp.indooratlas.IndoorAtlasProvider;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private FplanView _fplanView;
+    private ConstraintLayout _progressBar;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main_menu, menu);
+        //getMenuInflater().inflate(R.menu.activity_main_menu, menu);
         return true;
     }
 
@@ -55,60 +64,38 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
     }
 
+    public void goButtonClick(View view) {
+        EditText textView = findViewById(R.id.expoKeyEditText);
+        String text = textView.getText().toString();
+        String url = text.startsWith("https://") ? text : "https://" + text;
+        url = url.endsWith(".expofp.com") ? url : url + ".expofp.com";
+
+        _progressBar.setVisibility(View.VISIBLE);
+
+        FplanView.downloadZipToCache(url, this, new DownloadZipToCacheCallback() {
+            @Override
+            public void onCompleted(String htmlFilePath) {
+                _progressBar.setVisibility(View.INVISIBLE);
+                _fplanView.openFileFromCache(null, new Settings());
+            }
+
+            @Override
+            public void onError(String message) {
+                _progressBar.setVisibility(View.INVISIBLE);
+            }
+        });
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        com.expofp.fplan.models.Settings settings = new com.expofp.fplan.models.Settings()
-                //.withLocationProvider(new CrowdConnectedProvider(getApplication(), new com.expofp.crowdconnected.Settings("APP_KEY","TOKEN","SECRET", Mode.IPS_AND_GPS)))
-                //.withLocationProvider(new IndoorAtlasProvider(getApplication(), "API_KEY", "API_SECRET_KEY"))
-                .withEventsListener(new FplanEventsListener() {
-                    @Override
-                    public void onFpConfigured() {
-                    }
-
-                    @Nullable
-                    @Override
-                    public void onFpConfigureError(int errorCode, String description) {
-                    }
-
-                    @Override
-                    public void onBoothClick(@Nullable FloorPlanBoothBase booth) {
-                    }
-
-                    @Override
-                    public void onDirection(@Nullable Route route) {
-                    }
-
-                    @Override
-                    public void onMessageReceived(@Nullable String message) {
-                    }
-
-                    @Override
-                    public void onDetails(@Nullable Details details) {
-                    }
-
-                    @Override
-                    public void onBookmarkClick(Bookmark bookmark) {
-                    }
-
-                    @Override
-                    public void onCategoryClick(Category category) {
-                    }
-
-                    @Override
-                    public void onExhibitorCustomButtonClick(String externalId, int buttonNumber, String buttonUrl) {
-                    }
-
-                    @Override
-                    public void onCurrentPositionChanged(Location location) {
-
-                    }
-                });
-
-
+        _progressBar = findViewById(R.id.progressBar);
         _fplanView = findViewById(R.id.fplanView);
-        _fplanView.load("https://demo.expofp.com", settings);
+
+        //_fplanView.load("https://demo.expofp.com", new Settings());
     }
+
+
 }
